27fb27b0b646962d7836dce46a1dd93774b802cc
DianPham <152237146+DianPham@users.noreply.github.com>
2024-06-11 12:34:25 +0700

    first commit

---
+++ src/main/java/com/example/Bai2/service/OrderService.java
@@ -0,0 +1,17 @@
+package com.example.Bai2.service;
+
+import com.example.Bai2.model.Order;
+import com.example.Bai2.model.OrderDetail;
+import com.example.Bai2.repository.OrderDetailRepository;
+import com.example.Bai2.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+}
